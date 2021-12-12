1110d273d965f1b4c66b2cc61e30ec897461b2a0
Liel <lylp141@gmail.com>
2021-12-12 20:41:42 +0200

    addlogin

---
+++ finalProject/Controllers/ReturnObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace finalProject.Controllers
+{
+    public class ReturnObject
+    {
+        public bool Status { get; set; }
+        public string Error { get; set; }
+        public string Data { get; set; }
+
+    }
+}
