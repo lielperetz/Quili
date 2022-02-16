36dcb3f8b4a2e8cdcb939bc0ec89a5b1bb3e204f
Liel <lylp141@gmail.com>
2022-02-16 15:07:12 +0200

    add recipe card

--- app/src/app/show-recipe-details/show-recipe-details.component.ts
+++ app/src/app/show-recipe-details/show-recipe-details.component.ts
@@ -1,6 +1,8 @@
 import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { RecipesService } from '../services/recipes.service';
 
 @Component({
@@ -10,7 +12,8 @@ import { RecipesService } from '../services/recipes.service';
 })
 export class ShowRecipeDetailsComponent implements OnInit {
   showRecipe: Record<string, any>;
-  constructor(public recipes: RecipesService, public activeRoute: ActivatedRoute, public location: Location) { }
+  cardRecipe: string = "";
+  constructor(public httpclient: HttpClient, public recipes: RecipesService, public activeRoute: ActivatedRoute, public location: Location) { }
 
   ngOnInit(): void {
     this.activeRoute.params.subscribe(x => {
@@ -19,6 +22,13 @@ export class ShowRecipeDetailsComponent implements OnInit {
           (response: any) => {
             if (response.Status) {
               this.showRecipe = response.Data as Record<string, any>;
+              this.getRecipeCard("https://api.spoonacular.com/recipes/" + this.showRecipe?.id + "/card?apiKey=52b9142911034ec3b82f8d31cb7410ca").subscribe(
+                (res: any) => {
+                  if (res.status)
+                    this.cardRecipe = res.url;
+                  else
+                    console.log(res.message)
+                })
               console.log(this.showRecipe)
             }
             else
@@ -29,6 +39,10 @@ export class ShowRecipeDetailsComponent implements OnInit {
 
   goBack() {
     this.location.back();
+  }
+
+  getRecipeCard(url: string): Observable<object> {
+    return this.httpclient.get<object>(url);
   }
 }
 
