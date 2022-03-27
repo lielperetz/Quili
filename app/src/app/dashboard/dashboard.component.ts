599270a1899e65449dc6007f1530d373824213cf
hadasa0902 <hadasaregev@gmail.com>
2022-03-27 03:27:32 +0300

    sm-fix

--- app/src/app/dashboard/dashboard.component.ts
+++ app/src/app/dashboard/dashboard.component.ts
@@ -26,31 +26,29 @@ export class DashboardComponent implements OnInit {
     popList = []
     popSlideConfig = {
         slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
         infinite: true,
         dots: false,
         arrows: true,
-        autoplay: false,
+        autoplay: true,
+        autoplaySpeed: 8000,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
                 }
             }
         ]
