89e398eeecdc3205962b38c8b418ee0fb9f50c17
Liel <lylp141@gmail.com>
2022-03-28 17:56:40 +0300

    footer validation + change arrows color

--- app/src/app/footer/footer.component.ts
+++ app/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  feedback: Record<string, any> = { 'name': '', 'mail': '', 'message': '' }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  send() {
+    console.log(this.feedback)
+    Swal.fire({
+      title: 'Thank you for your feedback🤍',
+      icon: 'success',
+      iconColor: '#E16F26',
+      position: 'center',
+      showConfirmButton: false,
+      timer: 3000
+    })
+  }
 }
