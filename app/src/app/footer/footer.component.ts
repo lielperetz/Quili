df2a2cde6d02f4e0d1997ed7d1d17b8e66e2d4d8
Liel <lylp141@gmail.com>
2022-03-29 11:25:46 +0300

    send feedback to our email

--- app/src/app/footer/footer.component.ts
+++ app/src/app/footer/footer.component.ts
@@ -29,4 +29,31 @@ export class FooterComponent implements OnInit {
       timer: 3000
     })
   }
+
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+      const email = contactForm.value;
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      this.http.post('https://formspree.io/f/xknyvbwo',
+        { name: email.name, replyto: email.email, message: email.messages },
+        { 'headers': headers }).subscribe(
+          (response: any) => {
+            if (response.ok) {
+              contactForm.reset();
+              Swal.fire({
+                title: 'Thank you for your feedback🤍',
+                icon: 'success',
+                iconColor: '#E16F26',
+                position: 'center',
+                showConfirmButton: false,
+                timer: 3000
+              })
+            }
+            else {
+              console.log(response)
+            }
+          }
+        );
+    }
+  }
 }
